d7f3630ef92dba717e86c596a8bde441e4898e65
cnapop <cnjywr@163.com>
2024-03-21 11:17:27 +0800

    feat: user profile

--- comp3030j/views/__init__.py
+++ comp3030j/views/__init__.py
@@ -3,8 +3,8 @@ from flask import Flask
 def bind_views(app: Flask):
     from . import landing
     from . import dashboard
-    from .auth import auth
-    
+    from . import auth
+
     app.register_blueprint(landing.bp)
     app.register_blueprint(dashboard.bp)
     app.register_blueprint(auth.bp)
