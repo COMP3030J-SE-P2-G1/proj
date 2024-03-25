4ff4b6f4eaba2ac93ab05ab7cdd2436dae38d924
Aurora_Zoe <101802289+AuroraZoer@users.noreply.github.com>
2024-03-25 21:16:33 +0800

    feat: add chatbot templates

--- comp3030j/views/dashboard.py
+++ comp3030j/views/dashboard.py
@@ -2,9 +2,11 @@ from flask import Blueprint, render_template
 
 bp = Blueprint("dashboard", __name__, url_prefix="/dashboard")
 
+
 @bp.route('/')
 def dashboard():
     return render_template("page/dashboard/layout1/index.j2")
+
 
 @bp.route('/index')
 def index():
