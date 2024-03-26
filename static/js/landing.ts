cd8e75076a5aad3e6b2de723aff600f574d406b6
Meow King <mr.meowking@anche.no>
2024-03-26 11:58:58 +0800

    refactor(landing): Use aos to replace locomotive scroll

--- static/js/landing.ts
+++ static/js/landing.ts
@@ -2,17 +2,8 @@
  * Landing Page
  */
 
-// NOTE Since locomotive scroll v5 beta doesn't have good typescript support, we here
-// disable all the typescript checking for it when necessary 
-
 import { ready } from "./lib/utils";
 import { stickyHeaderAddShadowHandler } from "./lib/animation.ts";
-// @ts-expect-error https://github.com/locomotivemtl/locomotive-scroll/issues/550
-import LocomotiveScroll from 'locomotive-scroll';
-
-/**
- * apply shadow to sticky header
- */
 
 /**
  * Bind events to elements
@@ -25,31 +16,12 @@ function bindEvents(): void {
 }
 
 /**
- * Bind Locomotive Scroll Events
  */
-function bindLocomotiveScrollEvents() {
-    /* eslint-disable */
-    // @ts-expect-error locomotive scroll
-    window.addEventListener('hero-bg-0-scroll', (e: CustomEvent) => {
-        const { target , way } = e.detail;
-        // console.log(`target: ${target}`, `way: ${way}`);
-        if (way === "enter") {
-            // note: pls ensure `scale-125` is in tailwind's safelist
-            target.classList.add("scale-125");
-        } else {
-            target.classList.remove("scale-125");
-        }
-    });
-    /* eslint-enable */
 }
 
 (function () {
     ready(() => {
         bindEvents();
-
-        // eslint-disable-next-line
-        const locomotiveScroll = new LocomotiveScroll();
-        bindLocomotiveScrollEvents();
     });
 })();
 
