b98625fd7e62d97d999a0376153758f430d7555b
Yidi Wang <alexandrawang0925@gmail.com>
2024-03-28 11:15:28 +0800

    feat(auth): add validate_password for registration

--- comp3030j/auth/__init__.py
+++ comp3030j/auth/__init__.py
@@ -6,31 +6,30 @@ from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, ValidationError, TextAreaField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 from comp3030j.db.User import User
-
+import re
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username'
-                           , validators=[DataRequired(), Length(min=2, max=20)])
-    email = StringField('Email',
-                        validators=[DataRequired(), Email()])
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
-    confirm_password = PasswordField('Confirm Password',
-                                     validators=[DataRequired(), EqualTo('password')])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
 
-    # Catch errors when register duplicate information
     def validate_username(self, username):
-
         user = User.query.filter_by(username=username.data).first()
         if user:
             raise ValidationError('That username is taken. Please choose a different one')
 
     def validate_email(self, email):
-
         user = User.query.filter_by(email=email.data).first()
         if user:
             raise ValidationError('That email is taken. Please choose a different one')
 
+    def validate_password(self, password):
+        if not re.search(r"\d", password.data):  # Checks for at least one digit
+            raise ValidationError('The password must contain at least one digit.')
+        if not re.search(r"[A-Za-z]", password.data):  # Checks for at least one letter
+            raise ValidationError('The password must contain at least one alphabetic character.')
 
 class LoginForm(FlaskForm):
     email = StringField('Email',
