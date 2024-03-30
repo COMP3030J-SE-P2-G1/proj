fc0e230c9f3bfd66aaa34539ca453d646f3087a2
Aurora_Zoe <101802289+AuroraZoer@users.noreply.github.com>
2024-03-30 11:12:21 +0800

    fix: language

--- comp3030j/auth/__init__.py
+++ comp3030j/auth/__init__.py
@@ -5,7 +5,7 @@ import re
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, ValidationError
 from wtforms.validators import DataRequired, Length, Email, EqualTo
-
+from flask_babel import gettext
 from comp3030j.db.User import User
 from comp3030j.extensions import bcrypt
 
@@ -37,11 +37,15 @@ class RegistrationForm(FlaskForm):
 
 
 class LoginForm(FlaskForm):
-    email = StringField('Email',
-                        validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    remember = BooleanField('Remember Me')
-    submit = SubmitField('Login')
+    email = StringField(gettext('Email'), validators=[DataRequired(), Email()])
+    password = PasswordField(gettext('Password'), validators=[DataRequired()])
+    remember = BooleanField(gettext('Remember Me'))
+    submit = SubmitField(gettext('Login'))
+
+    def validate_password(self, field):
+        user = User.query.filter_by(email=self.email.data).first()
+        if user and not bcrypt.check_password_hash(user.password, field.data):
+            raise ValidationError('Invalid password. Please try again.')
 
 
 class ChangePassForm(FlaskForm):
