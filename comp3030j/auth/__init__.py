129013b4901f182bfd2c041cb5f7561591df2b51
Meow King <mr.meowking@anche.no>
2024-03-30 13:27:01 +0800

    feat: add translation for auth screen

--- comp3030j/auth/__init__.py
+++ comp3030j/auth/__init__.py
@@ -5,17 +5,17 @@ import re
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, ValidationError
 from wtforms.validators import DataRequired, Length, Email, EqualTo
-from flask_babel import gettext
 from comp3030j.db.User import User
 from comp3030j.extensions import bcrypt
+from comp3030j.util import _ltr
 
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
-    submit = SubmitField('Sign Up')
+    username = StringField(_ltr('Username'), validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField(_ltr('Email'), validators=[DataRequired(), Email()])
+    password = PasswordField(_ltr('Password'), validators=[DataRequired()])
+    confirm_password = PasswordField(_ltr('Confirm Password'), validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField(_ltr('Sign Up'))
 
     def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
@@ -29,23 +29,23 @@ class RegistrationForm(FlaskForm):
 
     def validate_password(self, password):
         if len(password.data) < 6:  # Check for minimum length
-            raise ValidationError('The password must be at least 6 characters long.')
+            raise ValidationError(_ltr('The password must be at least 6 characters long.'))
         if not re.search(r"\d", password.data):  # Checks for at least one digit
-            raise ValidationError('The password must contain at least one digit.')
+            raise ValidationError(_ltr('The password must contain at least one digit.'))
         if not re.search(r"[A-Za-z]", password.data):  # Checks for at least one letter
-            raise ValidationError('The password must contain at least one alphabetic character.')
+            raise ValidationError(_ltr('The password must contain at least one alphabetic character.'))
 
 
 class LoginForm(FlaskForm):
-    email = StringField(gettext('Email'), validators=[DataRequired(), Email()])
-    password = PasswordField(gettext('Password'), validators=[DataRequired()])
-    remember = BooleanField(gettext('Remember Me'))
-    submit = SubmitField(gettext('Sign In'))
+    email = StringField(_ltr('Email'), validators=[DataRequired(), Email()])
+    password = PasswordField(_ltr('Password'), validators=[DataRequired()])
+    remember = BooleanField(_ltr('Remember Me'))
+    submit = SubmitField(_ltr('Sign In'))
 
     def validate_password(self, field):
         user = User.query.filter_by(email=self.email.data).first()
         if user and not bcrypt.check_password_hash(user.password, field.data):
-            raise ValidationError('Invalid password. Please try again.')
+            raise ValidationError(_ltr('Invalid password. Please try again.'))
 
 
 class ChangePassForm(FlaskForm):
@@ -56,13 +56,15 @@ class ChangePassForm(FlaskForm):
 
     def validate_password(self, password):
         if len(password.data) < 6:  # Check for minimum length
-            raise ValidationError('The password must be at least 6 characters long.')
+            raise ValidationError(_ltr('The password must be at least 6 characters long.'))
         if not re.search(r"\d", password.data):  # Checks for at least one digit
-            raise ValidationError('The password must contain at least one digit.')
+            raise ValidationError(_ltr('The password must contain at least one digit.'))
         if not re.search(r"[A-Za-z]", password.data):  # Checks for at least one letter
-            raise ValidationError('The password must contain at least one alphabetic character.')
+            raise ValidationError(_ltr('The password must contain at least one alphabetic character.'))
 
     def validate_original_password(self, email, OriginalPassword):
         user = User.query.filter_by(email=email.data).first()
         if not user and bcrypt.check_password_hash(user.password, OriginalPassword):
-            raise ValidationError('Please check your password again!')
+            raise ValidationError(_ltr('Please check your password again!'))
+
+        
