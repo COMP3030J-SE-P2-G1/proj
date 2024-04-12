ce5bf7750eba016b5cc27bd7ca452bf6458e867c
Aurora_Zoe <101802289+AuroraZoer@users.noreply.github.com>
2024-04-12 10:51:54 +0800

    remove chatBot

--- comp3030j/views/__init__.py
+++ comp3030j/views/__init__.py
@@ -5,9 +5,7 @@ def bind_views(app: Flask):
     from . import landing
     from . import dashboard
     from . import auth
-    from . import chatbot
 
     app.register_blueprint(landing.bp)
     app.register_blueprint(dashboard.bp)
     app.register_blueprint(auth.bp)
-    app.register_blueprint(chatbot.bp)
--- comp3030j/views/chatbot.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, render_template
-
-bp = Blueprint("chatbot", __name__, url_prefix="/chatbot")
-
-
-@bp.route('/')
-def chatbot():
-    return render_template("page/chatbot/index.j2")
