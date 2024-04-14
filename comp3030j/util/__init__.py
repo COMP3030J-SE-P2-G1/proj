4eae53ec633402a506bca0e2eaed0ddcc8bc51ee
cnapop <cnjywr@163.com>
2024-04-14 09:50:21 +0800

    fix: rename the function

--- comp3030j/util/__init__.py
+++ comp3030j/util/__init__.py
@@ -12,7 +12,7 @@ def allowed_img(filename):
            filename.rsplit('.', 1)[1].lower() in {'png', 'jpg'}
 
 
-def allowed_img(filename):
+def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in {'csv', 'xlsx', 'xls'}
 
