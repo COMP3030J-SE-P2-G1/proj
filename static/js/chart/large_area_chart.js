e2b95c1320c5ba6a56fd659585e72b3bb5bdf896
DLow <zhengyifei66666@163.com>
2024-04-15 17:11:17 +0800

    add the line chart (based on the data of historical irish eletricity prices)

---
+++ static/js/chart/large_area_chart.js
@@ -0,0 +1,101 @@
+var dom = document.getElementById('ele');
+var myChart = echarts.init(dom, null, {
+  renderer: 'canvas',
+  useDirtyRect: false
+});
+var app = {};
+
+var option;
+
+async function updateChart() {
+  const response = await fetch('/static/others/historical-irish-electricity-prices.csv');
+  const text = await response.text();
+  const lines = text.trim().split('\n');
+  let dates = [];
+  let prices = [];
+
+  lines.slice(1).forEach(line => {
+    const parts = line.split(',');
+    let date = parts[0];
+    let price = parseFloat(parts[1]);
+    dates.push(date);
+    prices.push(price);
+  });
+
+  const selectedYear = document.getElementById('yearSelect').value;
+  const filteredDates = dates.filter(date => date.startsWith(selectedYear));
+  const filteredPrices = prices.filter((_, index) => dates[index].startsWith(selectedYear));
+
+  option = {
+    tooltip: {
+      trigger: 'axis',
+      position: function (pt) {
+        return [pt[0], '10%'];
+      }
+    },
+    title: {
+      left: 'center',
+      text: 'Irish Electricity Prices'
+    },
+    toolbox: {
+      feature: {
+        dataZoom: {
+          yAxisIndex: 'none'
+        },
+        restore: {},
+        saveAsImage: {}
+      }
+    },
+    xAxis: {
+      type: 'category',
+      boundaryGap: false,
+      data: filteredDates
+    },
+    yAxis: {
+      type: 'value',
+      boundaryGap: [0, '100%']
+    },
+    dataZoom: [
+      {
+        type: 'inside',
+        start: 0,
+        end: 10
+      },
+      {
+        start: 0,
+        end: 10
+      }
+    ],
+    series: [
+      {
+        name: 'Price (EUR)',
+        type: 'line',
+        symbol: 'none',
+        sampling: 'lttb',
+        itemStyle: {
+          color: 'rgb(255, 70, 131)'
+        },
+        areaStyle: {
+          color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+            {
+              offset: 0,
+              color: 'rgb(255, 158, 68)'
+            },
+            {
+              offset: 1,
+              color: 'rgb(255, 70, 131)'
+            }
+          ])
+        },
+        data: filteredPrices
+      }
+    ]
+  };
+
+  if (option && typeof option === 'object') {
+    myChart.setOption(option);
+  }
+}
+
+window.addEventListener('resize', myChart.resize);
+window.onload = updateChart;  // 在页面加载时初始化图表
