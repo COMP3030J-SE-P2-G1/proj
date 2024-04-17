50cd582c10b418b4f94061ab5fae56b64f8f131f
cnapop <cnjywr@163.com>
2024-04-17 18:42:49 +0800

    fix: route of create profile

--- comp3030j/views/dashboard.py
+++ comp3030j/views/dashboard.py
@@ -1,3 +1,6 @@
+import datetime
+import logging
+
 from flask import Blueprint, render_template, request, session, flash, jsonify
 from flask_login import login_required
 
@@ -33,7 +36,7 @@ def create_profile():
         # Add profile to the database
         user_to_update = User.query.filter_by(id=session['user_id']).first()
         if user_to_update:
-            file = profileForm.usage_file.data
+            file = request.files[profileForm.usage_file.name]
             usages = read_hourly_usage_csv(file)
             profile = Profile(user_id=session['user_id'], name=profileForm.name.data, desc=profileForm.desc.data,
                               start_time=profileForm.start_time.data, end_time=profileForm.end_time.data)
@@ -60,40 +63,41 @@ def create_profile():
             return jsonify({'status': 'error', 'message': 'Unavailable Account.'}), 400
     else:
         form_errors = {field: error[0] for field, error in profileForm.errors.items()}
+        print(form_errors)
         return jsonify({'status': 'error', 'message': 'Validation errors', 'errors': form_errors}), 400
 
-
-@bp.route('/update_usage', methods=['POST'])
-@login_required
-def update_usage():
-    if 'file' not in request.files:
-        flash(_ltr('Upload failed'), 'error')
-        return jsonify({'status': 'error', 'message': 'File upload failure'}), 400
-    file = request.files['file']
-    filetype = allowed_file(file.filename)
-    if file and filetype:
-        # update the avatar in the database
-        user_to_update = User.query.filter_by(id=session['user_id']).first()
-        if user_to_update:
-            if filetype == "csv":
-                usages = read_hourly_usage_csv(file)
-            profile = Profile(user_id=session['user_id'], name=str(file.filename.rsplit(".", 1)[0]),
-                              desc="Demo Profile")
-            db.session.add(profile)
-            db.session.commit()
-            for timestamp, value in usages.items():
-                if value is None:
-                    flash(_ltr('Not allowed data in:' + str(timestamp)), 'error')
-                    return jsonify(
-                        {'status': 'error', 'message': 'Not allowed data in the file.' + str(timestamp)}), 400
-                usage = Usage(time=timestamp, usage=value, profile_id=profile.id)
-                db.session.add(usage)
-            db.session.commit()
-            flash(_ltr('Usage data uploaded and saved.'), 'success')
-            return jsonify({'status': 'success', 'message': 'Usage updated successfully'}), 200
-        else:
-            flash(_ltr('Unavailable Account.'), 'error')
-            return jsonify({'status': 'error', 'message': 'Unavailable Account.'}), 400
-    else:
-        flash(_ltr('Upload failed'), 'error')
-        return jsonify({'status': 'error', 'message': 'Validation errors', 'errors': "unsupported file"}), 400
+#
+# @bp.route('/update_usage', methods=['POST'])
+# @login_required
+# def update_usage():
+#     if 'file' not in request.files:
+#         flash(_ltr('Upload failed'), 'error')
+#         return jsonify({'status': 'error', 'message': 'File upload failure'}), 400
+#     file = request.files['file']
+#     filetype = allowed_file(file.filename)
+#     if file and filetype:
+#         # update the avatar in the database
+#         user_to_update = User.query.filter_by(id=session['user_id']).first()
+#         if user_to_update:
+#             if filetype == "csv":
+#                 usages = read_hourly_usage_csv(file)
+#             profile = Profile(user_id=session['user_id'], name=str(file.filename.rsplit(".", 1)[0]),
+#                               desc="Demo Profile")
+#             db.session.add(profile)
+#             db.session.commit()
+#             for timestamp, value in usages.items():
+#                 if value is None:
+#                     flash(_ltr('Not allowed data in:' + str(timestamp)), 'error')
+#                     return jsonify(
+#                         {'status': 'error', 'message': 'Not allowed data in the file.' + str(timestamp)}), 400
+#                 usage = Usage(time=timestamp, usage=value, profile_id=profile.id)
+#                 db.session.add(usage)
+#             db.session.commit()
+#             flash(_ltr('Usage data uploaded and saved.'), 'success')
+#             return jsonify({'status': 'success', 'message': 'Usage updated successfully'}), 200
+#         else:
+#             flash(_ltr('Unavailable Account.'), 'error')
+#             return jsonify({'status': 'error', 'message': 'Unavailable Account.'}), 400
+#     else:
+#         flash(_ltr('Upload failed'), 'error')
+#         return jsonify({'status': 'error', 'message': 'Validation errors', 'errors': "unsupported file"}), 400
