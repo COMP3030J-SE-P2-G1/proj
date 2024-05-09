477ae7455d06c4505d4829a8cb375324acb24e98
Meow King <mr.meowking@anche.no>
2024-05-09 13:57:35 +0800

    fix: /user/profiles API

--- comp3030j/api/user.py
+++ comp3030j/api/user.py
@@ -1,6 +1,6 @@
 from comp3030j.db import db
 from comp3030j.db.Profile import Profile
-from flask import Blueprint
+from flask import Blueprint, jsonify
 from flask_login import current_user, login_required
 
 bp = Blueprint("api/user", __name__, url_prefix="/user")
@@ -10,6 +10,6 @@ bp = Blueprint("api/user", __name__, url_prefix="/user")
 def profiles():
     profiles = db.session.scalars(
         db.select(Profile)
-        .filter_by(u_id=current_user.id)).all()
-    return profiles
+        .filter_by(user_id=current_user.id)).all()
+    return jsonify([profile.to_dict() for profile in profiles])
     
