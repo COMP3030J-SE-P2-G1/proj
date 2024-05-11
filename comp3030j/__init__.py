e1eb369f78ce8870b111b580e2fef6f0265fa2a0
Aurora_Zoe <101802289+AuroraZoer@users.noreply.github.com>
2024-05-11 22:16:50 +0800

    feat:admin init

--- comp3030j/__init__.py
+++ comp3030j/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 from flask_babel import Babel, gettext, ngettext, lazy_gettext
+from flask_admin import Admin
 from flask_caching import Cache
 from comp3030j.db import initialize_db
 from comp3030j.views import bind_views
@@ -8,6 +9,7 @@ from .extensions import bcrypt, login_manager
 
 from redis import Redis
 
+
 def get_locale():
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
@@ -15,7 +17,10 @@ def get_locale():
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_pyfile('config.py')
 
-r = Redis(host=app.config["CACHE_REDIS_HOST"], port=app.config["CACHE_REDIS_PORT"], db=app.config["CACHE_REDIS_PORT"], socket_connect_timeout=300)
+admin = Admin(app, name='COMP3030J', template_mode='bootstrap4')
+
+r = Redis(host=app.config["CACHE_REDIS_HOST"], port=app.config["CACHE_REDIS_PORT"], db=app.config["CACHE_REDIS_PORT"],
+          socket_connect_timeout=300)
 try:
     r.ping()
     app.config["CACHE_TYPE"] = "RedisCache"
